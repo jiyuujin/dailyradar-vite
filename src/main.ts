cc23b0dca43fcc70aa2e66cb9839bc25e100e23f
jiyuujin <jiyuujinunite@gmail.com>
2021-01-31 18:32:28 +0900

    apollo setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { apolloClient } from './plugins/apollo'
 
-createApp(App).mount('#app')
+createApp(App).provide(DefaultApolloClient, apolloClient).mount('#app')
